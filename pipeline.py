4167915c9b79096b36dcbcbcadea8580ae256b37
Christian-ngnie <63919916+Christian-ngnie@users.noreply.github.com>
2025-04-07 13:18:15 +0000

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -49,7 +49,7 @@ CONFIG = {
         "fp16": True  # Enable mixed precision
     },
     "bertrend": {
-        "model_name": "./local_models/bert-base-multilingual-cased",
+        "model_name": "/mount/src/radar/local_models/bert-base-multilingual-cased",
         "temporal_weight": 0.4,
         "cluster_threshold": 0.35,
         "min_cluster_size": 4,
